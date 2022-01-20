bea1e42d796e63d57810f057af9790bc2f073090
MrFlufficans <dc.fluffy.cat@gmail.com>
2022-01-20 14:04:40 +0800

    I forgot to Change Numbers Hue Hue

--- ModificationCheckMaster.ps1
+++ ModificationCheckMaster.ps1
@@ -1,4 +1,4 @@
-$Version = 0.3
+$Version = 0.4
 $Update = 0
 $ConfirmUpdate = $null
 $Things = Invoke-RestMethod https://raw.githubusercontent.com/MrFlufficans/FluffyCreditMenuDK2/master/UtilVersion
